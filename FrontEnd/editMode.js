7fa243a457de5f89829837bb73d35b0a996ec1e4
Deborah Oudin <oudin.deborah.83@gmail.com>
2026-02-23 16:51:06 +0100

    edit mode modal creation

---
+++ FrontEnd/editMode.js
@@ -0,0 +1,26 @@
+//fetch récupération des travaux
+let works = [];
+fetch("http://localhost:5678/api/works")
+  .then((res) => res.json())
+  .then((data) => {
+    works = data;
+    displayWorks(works);
+    console.log(works);
+  });
+function displayWorks(list) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  list.forEach((work) => {
+    const figure = document.createElement("figure");
+    figure.innerHTML = ` <img src="${work.imageUrl}" alt="${work.title}"> <h3>${work.title}</h3> `;
+    gallery.appendChild(figure);
+  });
+}
+
+function openModal() {
+  const modal = document.createElement("div");
+  modal.classList.add("modalEdit");
+  modal.addEventListener("click", (e) => {
+    console.log("vous avez cliqué");
+  });
+}
